37a8c4a94d446f619d5764c4f5f4f0ee7da195f8
Navneet Malva <navneetmalva@gmail.com>
2025-06-19 18:01:24 +0530

    GlobalExceptionHandler Updated

--- src/main/java/net/malvanav/exception/GlobalExceptionHandler.java
+++ src/main/java/net/malvanav/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package net.malvanav.exception;
 
-import net.malvanav.domain.ResponseDomain;
 import net.malvanav.enums.EmployeeErrorCodes;
 import net.malvanav.util.ResponseUtil;
 import org.springframework.http.HttpStatus;
@@ -12,13 +11,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(value = Exception.class)
-  public ResponseEntity<ResponseDomain> handleException(Exception ex){
-    if(ex instanceof ApplicationException applicationException){
-      return new ResponseEntity<>(ResponseUtil.populateErrorResponseDomain(applicationException.getHttpStatus(), applicationException.getErrorDescription(), applicationException.getEmployeeErrorCodes()),
-          HttpStatus.INTERNAL_SERVER_ERROR);
+  public ResponseEntity<Object> handleException(Exception ex) {
+    if (ex instanceof ApplicationException applicationException) {
+      return ResponseUtil.populateErrorResponseDomain(applicationException.getHttpStatus(), applicationException.getErrorDescription(), applicationException.getEmployeeErrorCodes());
     }
-    return new ResponseEntity<>(ResponseUtil.populateErrorResponseDomain(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), EmployeeErrorCodes.INTERNAL_SYSTEM_ERROR),
-        HttpStatus.INTERNAL_SERVER_ERROR);
+    return ResponseUtil.populateErrorResponseDomain(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), EmployeeErrorCodes.INTERNAL_SYSTEM_ERROR);
   }
 
 }
--- src/main/java/net/malvanav/util/ResponseUtil.java
+++ src/main/java/net/malvanav/util/ResponseUtil.java
@@ -5,104 +5,112 @@ import net.malvanav.domain.ResponseDomain;
 import net.malvanav.domain.SuccessResponseDomain;
 import net.malvanav.enums.EmployeeErrorCodes;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Objects;
 
 public class ResponseUtil<T> {
 
-    /**
-     * populate success response with a message only
-     * @return
-     */
-    public static <T> ResponseDomain populateSuccessResponseDomain(String message){
-        ResponseDomain responseDomain = new ResponseDomain();
-        SuccessResponseDomain<T> successResponseDomain = new SuccessResponseDomain<T>();
-        successResponseDomain.setResponseMessage(message);
-        responseDomain.setSuccess(successResponseDomain);
-        return responseDomain;
-    }
+  /**
+   * populate success response with a message only
+   *
+   * @return
+   */
+  public static <T> ResponseDomain populateSuccessResponseDomain(String message) {
+    ResponseDomain responseDomain = new ResponseDomain();
+    SuccessResponseDomain<T> successResponseDomain = new SuccessResponseDomain<T>();
+    successResponseDomain.setResponseMessage(message);
+    responseDomain.setSuccess(successResponseDomain);
+    return responseDomain;
+  }
 
-    /**
-     * populate success response with data
-     * @param data
-     * @return
-     */
-    public static <T> ResponseDomain populateSuccessResponseDomain(T data){
-        ResponseDomain responseDomain = new ResponseDomain();
-        SuccessResponseDomain<T> successResponseDomain = new SuccessResponseDomain<T>();
-        successResponseDomain.setData(data);
-        responseDomain.setSuccess(successResponseDomain);
-        return responseDomain;
-    }
+  /**
+   * populate success response with data
+   *
+   * @param data
+   * @return
+   */
+  public static <T> ResponseDomain populateSuccessResponseDomain(T data) {
+    ResponseDomain responseDomain = new ResponseDomain();
+    SuccessResponseDomain<T> successResponseDomain = new SuccessResponseDomain<T>();
+    successResponseDomain.setData(data);
+    responseDomain.setSuccess(successResponseDomain);
+    return responseDomain;
+  }
 
-    /**
-     * populate success response with data and message
-     * @param data
-     * @param message
-     * @return
-     */
-    public static <T> ResponseDomain populateSuccessResponseDomain(T data, String message){
-        ResponseDomain responseDomain = new ResponseDomain();
-        SuccessResponseDomain<T> successResponseDomain = new SuccessResponseDomain<T>();
-        successResponseDomain.setData(data);
-        successResponseDomain.setResponseMessage(message);
-        responseDomain.setSuccess(successResponseDomain);
-        return responseDomain;
-    }
+  /**
+   * populate success response with data and message
+   *
+   * @param data
+   * @param message
+   * @return
+   */
+  public static <T> ResponseDomain populateSuccessResponseDomain(T data, String message) {
+    ResponseDomain responseDomain = new ResponseDomain();
+    SuccessResponseDomain<T> successResponseDomain = new SuccessResponseDomain<T>();
+    successResponseDomain.setData(data);
+    successResponseDomain.setResponseMessage(message);
+    responseDomain.setSuccess(successResponseDomain);
+    return responseDomain;
+  }
 
-    /**
-     * populate success response with List of data and message
-     * @param dataList
-     * @param message
-     * @return
-     */
-    public static <T> ResponseDomain populateSuccessResponseDomain(List<T> dataList, String message){
-        ResponseDomain responseDomain = new ResponseDomain();
-        SuccessResponseDomain<T> successResponseDomain = new SuccessResponseDomain<T>();
-        successResponseDomain.setDataList(dataList);
-        successResponseDomain.setResponseMessage(message);
-        responseDomain.setSuccess(successResponseDomain);
-        return responseDomain;
-    }
+  /**
+   * populate success response with List of data and message
+   *
+   * @param dataList
+   * @param message
+   * @return
+   */
+  public static <T> ResponseDomain populateSuccessResponseDomain(List<T> dataList, String message) {
+    ResponseDomain responseDomain = new ResponseDomain();
+    SuccessResponseDomain<T> successResponseDomain = new SuccessResponseDomain<T>();
+    successResponseDomain.setDataList(dataList);
+    successResponseDomain.setResponseMessage(message);
+    responseDomain.setSuccess(successResponseDomain);
+    return responseDomain;
+  }
 
-    /**
-     * populate error response with data and errorMessage
-     * @param httpStatus
-     * @param errorMessage
-     * @return
-     */
-    public static <T> ResponseDomain populateErrorResponseDomain(HttpStatus httpStatus, String errorMessage){
-        ResponseDomain responseDomain = new ResponseDomain();
-        ErrorResponseDomain errorResponseDomain = new ErrorResponseDomain();
-        errorResponseDomain.setErrorMessage(errorMessage);
-        errorResponseDomain.setStatusCodeMessage(httpStatus);
-        errorResponseDomain.setStatusCode(httpStatus.value());
-        responseDomain.setError(errorResponseDomain);
-        return responseDomain;
-    }
+  /**
+   * populate error response with data and errorMessage
+   *
+   * @param httpStatus
+   * @param errorMessage
+   * @return
+   */
+  public static <T> ResponseDomain populateErrorResponseDomain(HttpStatus httpStatus, String errorMessage) {
+    ResponseDomain responseDomain = new ResponseDomain();
+    ErrorResponseDomain errorResponseDomain = new ErrorResponseDomain();
+    errorResponseDomain.setErrorMessage(errorMessage);
+    errorResponseDomain.setStatusCodeMessage(httpStatus);
+    errorResponseDomain.setStatusCode(httpStatus.value());
+    responseDomain.setError(errorResponseDomain);
+    return responseDomain;
+  }
 
-    /**
-     * Populate error response domain with custom error codes and description
-     * @param httpStatus
-     * @param errorMessage
-     * @param errorCodes
-     * @return
-     * @param <T>
-     */
-    public static <T> ResponseDomain populateErrorResponseDomain(HttpStatus httpStatus, String errorMessage, EmployeeErrorCodes errorCodes){
-        ResponseDomain responseDomain = new ResponseDomain();
-        ErrorResponseDomain errorResponseDomain = new ErrorResponseDomain();
-        errorResponseDomain.setStatusCode(httpStatus.value());
-        errorResponseDomain.setStatusCodeMessage(httpStatus);
-        errorResponseDomain.setErrorMessage(errorMessage);
-        if(Objects.nonNull(errorCodes)){
-            errorResponseDomain.setErrorCode(errorCodes.getCode());
-            errorResponseDomain.setErrorDescription(errorCodes.getDescription());
-            errorResponseDomain.setErrorTitle(errorCodes.getTitle());
-        }
-        responseDomain.setError(errorResponseDomain);
-        return responseDomain;
+
+  /**
+   * Creates a {@link ResponseEntity} containing an error response domain populated with the given HTTP status,
+   * error message, and optional custom employee error codes.
+   *
+   * @param httpStatus   the HTTP status to set in the error response
+   * @param errorMessage the error message describing the error
+   * @param errorCodes   an optional {@link EmployeeErrorCodes} enum containing custom error code details; may be null
+   * @return a {@link ResponseEntity} containing a {@link ResponseDomain} with the error details set
+   */
+  public static ResponseEntity<Object> populateErrorResponseDomain(HttpStatus httpStatus, String errorMessage, EmployeeErrorCodes errorCodes) {
+    ResponseDomain responseDomain = new ResponseDomain();
+    ErrorResponseDomain errorResponseDomain = new ErrorResponseDomain();
+    errorResponseDomain.setStatusCode(httpStatus.value());
+    errorResponseDomain.setStatusCodeMessage(httpStatus);
+    errorResponseDomain.setErrorMessage(errorMessage);
+    if (Objects.nonNull(errorCodes)) {
+      errorResponseDomain.setErrorCode(errorCodes.getCode());
+      errorResponseDomain.setErrorDescription(errorCodes.getDescription());
+      errorResponseDomain.setErrorTitle(errorCodes.getTitle());
     }
+    responseDomain.setError(errorResponseDomain);
+    return new ResponseEntity<>(responseDomain, responseDomain.getError().getStatusCodeMessage()) ;
+  }
 
 }
